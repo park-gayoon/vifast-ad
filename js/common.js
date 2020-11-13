4081a38f89eef1fd7e8b4727a8f640b30d713b2b
parkgayoon <wlalstm1@gmail.com>
2020-11-13 19:37:52 +0900

    UI element 가이드페이지 작업중, 프로젝트 파일생성, js 추가, sprite psd 추가

--- js/common.js
+++ js/common.js
@@ -1,16 +1,23 @@
 ﻿/**
- * Created by Gayoon on 2020.11.11
+ * Created by Gayoon on 2020.11.12
  * @author : Publishing Team.
  * @dependency : jquery ver 1.12.4+
  * @env : PC
  */
 
 $(document).ready(function(){
-    /* modal */
+    // select box
+    $(".sel select").each(function () {
+        if($(this).is(':disabled')){
+            $(this).wrapAll(" <div class='select--disabled'></div>");
+        }
+    });
+    $('.sel select').heapbox();
+
+    // modal
     var $panel = $(".box_popup-panel");
     $(".btn_popup_open").on("click", function(e) {
         $panel.fadeIn(350);
-        alert('tgus');
     });
     $(".btn_popup_close").on("click", popupClose);
 
@@ -20,7 +27,7 @@ $(document).ready(function(){
         e.preventDefault();
     }
 
-    /* 햄버거버튼 클릭 시 */
+    // 햄버거버튼 클릭 시
     $('.btn_gnb-open').click(function(){
         var tar = $('.layout_wrap');
         var tarClass = tar.attr('class');
@@ -32,7 +39,7 @@ $(document).ready(function(){
         //$('.size-s .btn_gnb-menu').removeClass('on').next().css('display','none');
     });
 
-    /* menu 클릭 시 */
+    // GNB menu 클릭 시
     $('.btn_gnb-menu').click(function(){
         if ($(this).hasClass('on')) {
             slideUp();
@@ -45,7 +52,7 @@ $(document).ready(function(){
             $('.btn_gnb-menu').removeClass('on').next().slideUp();
         };
 
-        /* 접힌 메뉴 클릭 시 */
+        // 접힌 메뉴 클릭 시
         var btnTar = $('.layout_wrap');
         var btnTarClass = btnTar.attr('class');
         if(btnTarClass != 'layout_wrap size-b') {
@@ -59,10 +66,10 @@ $(document).ready(function(){
 
     // toggle 공통
     $('.btn_toggle').click(function() {
-        $(this).toggleClass('is-active');
+        $('.box_toggle-cont').toggleClass('is-active');
     });
 
-    //tab
+    // tab
     $('.tab_type li button, .btn_focus-tab').click(function () {
         $('.tab_type .active').removeClass('active');
         $(this).addClass('active');
@@ -72,8 +79,8 @@ $(document).ready(function(){
         $('.' + contentid).addClass('active');
     });
 
-    //datepicker
-    $(".datepicker").datepicker({
+    // datepicker
+    $("input.datepicker").datepicker({
         showOn: "button",
         dateFormat: "yy-mm-dd",
         buttonText: "Select date",
@@ -82,9 +89,21 @@ $(document).ready(function(){
         showOtherMonths: true,
         monthNamesShort: [ "1","2","3","4","5","6","7","8","9","10","11","12" ],
         dayNamesMin: [ "일","월","화","수","목","금","토" ],
-        beforeShow: function() {
+        /*beforeShow: function() {
             const $datepickerDiv = $("#ui-datepicker-div");
             $datepickerDiv.after('<div class="ui-widget-overlay ui-front" />');
+        },*/
+        beforeShow: function(input, inst)
+        {
+            var calendar = inst.dpDiv;
+            setTimeout(function() {
+                calendar.position({
+                    my: 'right top',
+                    at: 'right bottom',
+                    collision: 'none',
+                    of: input
+                });
+            }, 1);
         },
         onSelect: function() {
             $('.ui-widget-overlay.ui-front').remove();
